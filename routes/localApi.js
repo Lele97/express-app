007e6e10358c9865eb64a9dae101da71bad3f086
Gabriele Grandinetti <gabriele.grandinetti@hotmail.com>
2023-12-07 15:43:13 +0100

    Second Commit

--- routes/localApi.js
+++ routes/localApi.js
@@ -2,7 +2,9 @@ const express = require("express")
 const router = express.Router();
 
 router.get("/",(req, res)=>{
-    res.render("api")
+    res.render("api", {
+       message:"List api local" 
+    })
 })
 
 module.exports = router
